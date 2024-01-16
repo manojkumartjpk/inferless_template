51b5c22d460ca429f2957541a4700029a5411367
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-01-16 10:18:46 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,19 @@
+INPUT_SCHEMA = {
+    'prompt': {
+        'type': str,
+        'required': True
+				'shape': [1]
+    },
+    'negative_prompt': {
+        'type': str,
+        'required': False,
+        'example': None
+				'shape': [1]
+    },
+    'width': {
+        'type': int,
+        'required': False,
+        'example': [ 768 ,  512 ],
+				'shape': [2]
+    },
+}
