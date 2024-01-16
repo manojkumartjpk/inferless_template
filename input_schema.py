1b9291590c4200e7a4b1d0b924ac608ed40a48ba
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-01-16 10:21:27 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,19 +1,19 @@
 INPUT_SCHEMA = {
-    'prompt': {
+    "prompt": {
         'type': str,
-        'required': True
-				'shape': [1]
+        'required': True,
+	'shape': [1]
     },
     'negative_prompt': {
         'type': str,
         'required': False,
-        'example': None
-				'shape': [1]
+        'example': None,
+	'shape': [1]
     },
     'width': {
         'type': int,
         'required': False,
         'example': [ 768 ,  512 ],
-				'shape': [2]
+	'shape': [2]
     },
 }
